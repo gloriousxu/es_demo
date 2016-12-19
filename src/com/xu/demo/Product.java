e78fe599141375f4d893f91a2734dc9315b7aa64
xuchao <gloriousxu@163.com>
2016-12-19 16:49:03 +0800

    set get product

--- src/com/xu/demo/Product.java
+++ src/com/xu/demo/Product.java
@@ -6,4 +6,22 @@ package com.xu.demo;
  */
 //产品类
 public class Product {
+    private int id;
+    private String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
