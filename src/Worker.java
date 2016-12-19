81d23de5fd933183e1144740719be44e7545215e
unknown <xuchao@Koolearn-inc.com>
2016-12-19 12:09:52 +0800

    idea demo

---
+++ src/Worker.java
@@ -0,0 +1,6 @@
+/**
+ * Created by xuchao on 2016/12/16.
+ */
+public interface Worker {
+    public int dosome();
+}
---
+++ src/com/xu/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.xu.demo;
+
+/**
+ * Created by xuchao on 2016/12/19.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
