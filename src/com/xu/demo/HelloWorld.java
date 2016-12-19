80e67b8bf7227b78e154cd4763eb2d83e7b16927
xuchao <gloriousxu@163.com>
2016-12-19 12:30:38 +0800

    new person.java

--- src/com/xu/demo/HelloWorld.java
+++ src/com/xu/demo/HelloWorld.java
@@ -1,10 +1,9 @@
 package com.xu.demo;
 
-/**
- * Created by xuchao on 2016/12/19.
- */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("aaa");
+
     }
 }
---
+++ src/com/xu/demo/Person.java
@@ -0,0 +1,6 @@
+package com.xu.demo;
+
+public class Person {
+    public int age;
+
+}
